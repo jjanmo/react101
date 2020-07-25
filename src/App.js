0cb4a3ee3f5944468dfd98237efc426fe3a63258
jjanmo <jjanmo07@hanmail.net>
2020-07-25 18:47:31 +0900

    Complete movie list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 //GOAL : show movielist
 
@@ -15,43 +16,59 @@ class App extends React.Component {
         };
     }
 
+    changeObj(array) {
+        const newObj = {}
+        array.forEach(obj => {
+            newObj[obj.id] = obj.name;
+        })
+        return newObj;
+    }
+
     async getMovieList() {
         const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+        const { data: { genres } } = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+        const genresFormat = this.changeObj(genres);
+        results.forEach(movie => {
+            movie.genres = movie.genre_ids.reduce(((list, id) => list.concat(genresFormat[id])), []);
+        })
+        console.log(results);
         this.setState({
             isLoading: false,
             movieList: results
         });
     }
 
-    componentDidMount() {
-        this.getMovieList();
-    }
-
-    renderMovies(list) {
-        return list.map(movie => {
+    renderMovies(movieList) {
+        return movieList.map(movie => {
             return (
                 <Movie
-                    key={movie.id}
-                    id={movie.id}
-                    title={movie.title}
-                    poster={movie.poster_path}
-                    rating={movie.vote_average}
+                    key={ movie.id }
+                    id={ movie.id }
+                    title={ movie.title }
+                    overview={ movie.overview }
+                    poster={ movie.poster_path }
+                    rating={ movie.vote_average }
+                    release={ movie.release_date }
+                    genres={ movie.genres }
                 />
             );
         })
     }
 
+    componentDidMount() {
+        this.getMovieList();
+    }
+
     render() {
         const { isLoading, movieList } = this.state;
         return (
-            <h1>{
+            <div className="wrapper">{
                 isLoading
                     ? 'Loading...'
                     : this.renderMovies(movieList)
-            }</h1>
+            }</div>
         );
     }
 }
-
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import Proptypes from 'prop-types'
+import Proptypes from 'prop-types';
+import './Movie.css';
 
-const POSTER_BASIC_PATH = 'https://image.tmdb.org/t/p/w185';
+const POSTER_BASIC_PATH = 'https://image.tmdb.org/t/p/w154';
 
-function Movie({ id, title, poster, rating }) {
+function Movie({ id, title, overview, poster, rating, release, genres }) {
     return (
-        <section id={id}>
-            <h5>{title}</h5>
-            <img src={`${POSTER_BASIC_PATH}${poster}`} alt={title} title={title} />
-            <div>{rating} / 10</div>
+        <section id={ id }>
+            <img src={ `${POSTER_BASIC_PATH}${poster}` } alt={ title } title={ title } />
+            <div className="movie-detail">
+                <h3>{ title }</h3>
+                <span className="release-year">{ release.split('-').join('.') }</span>🎈
+                <span className="rating">{ rating } / 10</span>
+                <Genres genres={ genres } />
+                <div className="overview">{ `${overview.slice(0, 140)}...` }</div>
+            </div>
         </section>
     );
+}
+
+function Genres({ genres }) {
+    return <ul>{ genres.map((genre, index) => <li key={ index }>{ genre }</li>) }</ul>;
 }
 
 Movie.propTypes = {
     id: Proptypes.number.isRequired,
     title: Proptypes.string.isRequired,
+    overview: Proptypes.string.isRequired,
     poster: Proptypes.string.isRequired,
-    rating: Proptypes.number.isRequired
+    rating: Proptypes.number.isRequired,
+    release: Proptypes.string.isRequired,
+    genres: Proptypes.array.isRequired
 }
 
 export default Movie;
